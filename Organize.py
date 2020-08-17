e320e9878f0b1ce200aa6e37a5d7e3823906a4d4
PsSarvna <pssarvna.kumr@gmail.com>
2020-08-17 15:48:37 +0530

    Added Unsort Function and Return button
    Added Feature to unsort files
    fixed bugs
       - Radio button default state changed and warning msg box if none of the radio button is selected
      - Reset button is added to return to initial state

--- Organize.py
+++ Organize.py
@@ -3,6 +3,7 @@ import shutil
 import datetime
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
 
 #--------------------------------------------------------------------------------------------------------------------
 window = tk.Tk()
@@ -29,11 +30,28 @@ def Pre_unsort():
 	Des_browse.configure(state = "disabled")
 	Des_entry.configure(state = "disabled")
 
+def sort_pre():
+	Des_browse.configure(state = "normal")
+	Des_entry.configure(state = "normal")
+
 def sort_files_button():
 	src = Src_entry.get()
 	des = Des_entry.get()
 	datetime = daterb.get()
+	if len(datetime) < 2:
+		tk.messagebox.showerror("Error!", "Select any one type.")
+		return
 	Inc_sub = Inc_check.get()
+	Orig_src = src
+	print(datetime)
+	if datetime == "unsort":
+		fname = "UnSorted"
+		if Orig_src == src :
+			if fname not in os.listdir(src):
+				os.mkdir(os.path.join(src, fname))
+			Unsort_files(src , src, Inc_sub )
+			return
+
 	if datetime == "ext":
 		Ext_Sort = True
 		Ext_Spec = False
@@ -44,9 +62,40 @@ def sort_files_button():
 		Ext_Sort = False
 		Ext_Spec = False
 	Sort_files(src ,des, datetime , Inc_sub, Ext_Sort ,Ext_Spec)
-	
 
-	
+def Unsort_files(Orig_src , src, Inc_sub ):
+
+	fname = "UnSorted"
+	files = os.listdir(src)
+
+	for file in files:
+		extension = os.path.splitext(file)[1]
+		if os.path.basename(file) == fname:
+			continue
+		
+		if os.path.isfile(os.path.join(src , file)):
+			#if Ext_Unsort:
+			#	if extension not in Un_Ext:
+			#		continue
+			shutil.move(os.path.join(src , file) , os.path.join(Orig_src, fname))
+		else:
+			if Inc_sub:
+				Unsort_files(Orig_src, os.path.join(src , file), Inc_sub )
+				try:
+					os.rmdir(os.path.join(src , file))
+				except:
+					pass
+
+
+def Reset_Variables():
+	Radio_btn1.deselect()
+	Radio_btn2.deselect()
+	Radio_btn4.deselect()
+	Radio_btn3.deselect()
+	Radio_btn5.deselect()
+	Des_entry.delete(0, tk.END)
+	Src_entry.delete(0, tk.END)
+	Inc_check.set(False)
 
 
 #--------------------------------------------------------------------------------------------------------------------
@@ -136,25 +185,33 @@ def Sort_files(src, des, Datetime , Inc_sub, Ext_Sort ,Ext_Spec ):
 
 gui_title = tk.Label(text = "Organize files" , fg = "black", font= ("Fixedsys",24) )
 gui_title.place(x =200 , y = 30)
+linev = tk.Canvas(window,width = 1 , height = 195 , bg = "#DCDCDC")
+linev.place(x =280, y = 100)
+lineh = tk.Canvas(window,width = 500 , height = 1 , bg = "#DCDCDC")
+lineh.place(x =40, y = 295)
+lineh = tk.Canvas(window,width = 500 , height = 1 , bg = "#DCDCDC")
+lineh.place(x =40, y = 395)
 
-
-Radio_btn1 = tk.Radiobutton(window,text="Sort - Month/Year" , value = "%b_%Y" , variable = daterb)
+Radio_btn1 = tk.Radiobutton(window,text="Sort - Month/Year" , value = "%b_%Y" , variable = daterb,tristatevalue = "x",command = sort_pre)
 Radio_btn1.place(x =100 , y = 130)
-Radio_btn2 = tk.Radiobutton(window,text="Sort - Year      ." , value = "%Y", variable = daterb)
+Radio_btn2 = tk.Radiobutton(window,text="Sort - Year      ." , value = "%Y", variable = daterb,tristatevalue = "x",command = sort_pre)
 Radio_btn2.place(x =100 , y = 170)
-Radio_btn3 = tk.Radiobutton(window,text="Sort - Ext       ." , value = "ext", variable = daterb)
+Radio_btn3 = tk.Radiobutton(window,text="Sort - Ext       ." , value = "ext", variable = daterb,tristatevalue = "x",command = sort_pre)
 Radio_btn3.place(x =100 , y = 210)
-Radio_btn4 = tk.Radiobutton(window,text="Sort - Ext-Spec  " , value = "extspec", variable = daterb)
+Radio_btn4 = tk.Radiobutton(window,text="Sort - Ext-Spec  " , value = "extspec", variable = daterb,tristatevalue = "x",command = sort_pre)
 Radio_btn4.place(x =100 , y = 250)
+Radio_btn5 = tk.Radiobutton(window,text="UnSort" , value = "unsort", variable = daterb , command = Pre_unsort,tristatevalue = "x")
+Radio_btn5.place(x =375 , y = 130)
+
 
 Src_entry = tk.Entry(window ,width = 40)
 Src_entry.place(x =50 , y = 320)
 Src_browse = tk.Button(window,text = "Browse" , command = get_src_direct)
 Src_browse.place(x =300 , y = 315)
-Submit = tk.Button(window,text = "Submit" , command = sort_files_button )
-Submit.place(x =435 , y = 330)
-
-
+Submit = tk.Button(window,text = "Submit" ,width = 8 , height = 1 , command = sort_files_button )
+Submit.place(x =435 , y = 315)
+Reset = tk.Button(window,text = "Reset " ,width = 4, command = Reset_Variables )
+Reset.place(x =448 , y = 355)
 
 
 Des_entry = tk.Entry(window,width = 40)
@@ -169,37 +226,4 @@ window.mainloop()
 
 
 #--------------------------------------------------------------------------------------------------------------------
-"""
-Radio_btn5 = tk.Radiobutton(text="UnSort" , value = "Unsort", variable = un_sort , command = Pre_unsort)
-Radio_btn5.pack()
 
-
-Un_Ext = [".jpg"]
-
-
-def Unsort_files(src):
-
-	fname = "UnSorted"
-	if Orig_src == src :
-		if fname not in os.listdir(Orig_src):
-			os.mkdir(os.path.join(Orig_src, fname))
-	files = os.listdir(src)
-
-	for file in files:
-		extension = os.path.splitext(file)[1]
-		if os.path.basename(file) == fname:
-			continue
-		
-		if os.path.isfile(os.path.join(src , file)):
-			if Ext_Unsort:
-				if extension not in Un_Ext:
-					continue
-			shutil.move(os.path.join(src , file) , os.path.join(Orig_src, fname))
-		else:
-			Unsort_files(os.path.join(src , file))
-			try:
-				os.rmdir(os.path.join(src , file))
-			except:
-				pass
-
-"""
