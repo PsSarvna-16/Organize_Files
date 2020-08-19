f73e18fd4d0fd307c5197d06b2eb0fa4760d0fd3
PsSarvna <pssarvna.kumr@gmail.com>
2020-08-19 21:09:45 +0530

    Radiobutton Replaced
    Radiobuttons are replaced with Combo Box

--- Organize.py
+++ Organize.py
@@ -4,23 +4,28 @@ import datetime
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
+from tkinter import ttk
 
-#--------------------------------------------------------------------------------------------------------------------
 window = tk.Tk()
 window.title("PsSarvna")
 window.geometry("500x400")
 window.minsize(width = "600" , height = "500")
 window.maxsize(width = "600" , height = "500")
 
-daterb = tk.StringVar()
+#--------------------------------------------------------------------------------------------------------------------
+
 Inc_check = tk.BooleanVar()
 Over_check = tk.BooleanVar()
 copy = tk.BooleanVar()
-Files_Skipped = 0
-Files_Moved = 0
-Files_OverWrited = 0
-Files_Copied = 0
 
+Images = ["Images" , ".jpg" , ".png" , ".gif" , "jpeg"]
+Videos = ["Videos" , ".mp4" , ".mpeg" , ".avi" , ".mkv"]
+Audio = ["Audio" , ".mp3" , ".wma" , ".amr" ]
+Softwares = ["Softwares" , ".exe"]
+
+
+Ext = [Images , Videos , Audio, Softwares ]
+#--------------------------------------------------------------------------------------------------------------------
 
 def get_src_direct():
 	src = filedialog.askdirectory()
@@ -40,12 +45,21 @@ def sort_pre():
 	Des_browse.configure(state = "normal")
 	Des_entry.configure(state = "normal")
 
+def Reset_Variables():
+	Des_entry.delete(0, tk.END)
+	Src_entry.delete(0, tk.END)
+	Combo_sort.set('')
+	Inc_check.set(False)
+	Over_check.set(False)
+	copy.set(False)
+
+
 def sort_files_button():
 	src = Src_entry.get()
 	des = Des_entry.get()
-	datetime = daterb.get()
+	Sort_type = Combo_sort.get()
 	Copy  = copy.get()
-	if len(datetime) < 2:
+	if len(Sort_type) < 2:
 		tk.messagebox.showerror("Error!", "Select any one type.")
 		return
 	Inc_sub = Inc_check.get()
@@ -59,20 +73,64 @@ def sort_files_button():
 			Unsort_files(src , src, Inc_sub  , OverWrite, Copy )
 			return
 
-	if datetime == "ext":
-		Ext_Sort = True
-		Ext_Spec = False
-	elif datetime == "extspec":
-		Ext_Spec = True
-		Ext_Sort = False
-	else:
-		Ext_Sort = False
-		Ext_Spec = False
-	Sort_files(src ,des, datetime , Inc_sub, Ext_Sort ,Ext_Spec, OverWrite ,Copy )
-	print(Files_Copied)
-	print(Files_Moved)
-	print(Files_OverWrited)
-	print(Files_Skipped)
+	Sort_files(src ,des, Inc_sub,Sort_type, OverWrite ,Copy)
+
+#--------------------------------------------------------------------------------------------------------------------
+
+
+def Sort_files(src, des,  Inc_sub,Sort_type ,OverWrite ,Copy  ): 
+	
+	files = os.listdir(src)
+	for file in files:
+		extension = os.path.splitext(file)[1]
+		if os.path.isfile(os.path.join(src , file)):
+			
+			if Sort_type == "Extensions":
+				fname = os.path.splitext(file)[1].split(".")[1]
+			elif Sort_type == "FileType":
+				find = False
+				for lists in Ext:
+					if extension in lists:
+						fname = lists[0]
+						find = True
+				if not find:
+					fname = "Others"
+					find = False
+			elif Sort_type == "Mon_Year":
+				dtime = datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(src , file)))
+				fname = dtime.strftime("%b_%Y")
+			elif Sort_type == "Year":
+				dtime = datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(src , file)))
+				fname = dtime.strftime("%Y")
+			else:
+				return 
+
+
+			if fname not in os.listdir(des):
+					os.mkdir(os.path.join(des , fname))
+
+			try :
+				if not Copy:
+					shutil.move(os.path.join(src , file) , os.path.join(des , fname))
+				else:
+					try:
+						shutil.copy2(os.path.join(src , file) ,os.path.join(os.path.join(des , fname),file))
+					except:
+						pass
+			except:
+				if OverWrite:
+					try:
+						shutil.move(os.path.join(src , file) , os.path.join(os.path.join(des , fname),file))
+					except:
+						pass
+		else:
+			if Inc_sub:
+				if os.path.isdir(os.path.join(src , file)):
+					Sort_files(os.path.join(src , file),des, Inc_sub,Sort_type, OverWrite ,Copy  )
+					try:
+						os.rmdir(os.path.join(src , file))
+					except:
+						pass
 
 
 def Unsort_files(Orig_src , src, Inc_sub , OverWrite,Copy):
@@ -112,117 +170,6 @@ def Unsort_files(Orig_src , src, Inc_sub , OverWrite,Copy):
 					pass
 
 
-def Reset_Variables():
-	Radio_btn1.deselect()
-	Radio_btn2.deselect()
-	Radio_btn4.deselect()
-	Radio_btn3.deselect()
-	Radio_btn5.deselect()
-	Des_entry.delete(0, tk.END)
-	Src_entry.delete(0, tk.END)
-	Inc_check.set(False)
-	Over_check.set(False)
-	copy.set(False)
-
-
-#--------------------------------------------------------------------------------------------------------------------
-
-Images = ["Images" , ".jpg" , ".png" , ".gif" , "jpeg"]
-Videos = ["Videos" , ".mp4" , ".mpeg" , ".avi" , ".mkv"]
-Audio = ["Audio" , ".mp3" , ".wma" , ".amr" ]
-Softwares = ["Softwares" , ".exe"]
-
-Ext = [Images , Videos , Audio, Softwares ]
-
-#--------------------------------------------------------------------------------------------------------------------
-
-def Sort_files(src, des, Datetime , Inc_sub, Ext_Sort ,Ext_Spec , OverWrite,Copy ): 
-	
-	"""
-	Function Process Step by step:
-			1. get lis of files in Src folder
-			2. Iterate through files
-			3. Get extension of each file
-			4. if the file is directory (This Works if Inc_Sub is True)
-					- call same function as recursive using foldername as source
-			5. if it is file.
-			6. Types of Sorting (According to Boolean inputs) - Determine Folder names
-					a)  Ext_Spec = True
-							- Sort files as (Audio ,Video , Images) by checking Predefined Ext list
-					b) Ext_Sort  =True
-							- Sort files as their extension of the file.
-					c)  Else 
-							- Sort with Modified time (month and year / year - determined by Datetime) 
-			7. check folder is presen or Create folder
-			8. Move files to destination
-			9.If there is Conflicts:
-				--Create Conflicts folder
-				--create folder with mtime 
-				--move to that folder
-	"""
-	
-	files = os.listdir(src)
-	for file in files:
-		extension = os.path.splitext(file)[1]
-		if os.path.isfile(os.path.join(src , file)):
-			if Ext_Spec:
-				find = False
-				for lists in Ext:
-					if extension in lists:
-						fname = lists[0]
-						find = True
-				if not find:
-					fname = "Others"
-					find = False
-
-			elif Ext_Sort:
-				fname = os.path.splitext(file)[1].split(".")[1]
-			else:
-				dtime = datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(src , file)))
-				fname = dtime.strftime(Datetime)
-
-			if fname not in os.listdir(des):
-					os.mkdir(os.path.join(des , fname))
-
-			try :
-				if not Copy:
-					shutil.move(os.path.join(src , file) , os.path.join(des , fname))
-					Files_Moved += 1
-				else:
-					try:
-						shutil.copy2(os.path.join(src , file) ,os.path.join(os.path.join(des , fname),file))
-						Files_Copied += 1
-					except:
-						Files_Skipped += 1
-						pass
-			except:
-				if OverWrite:
-					try:
-						shutil.move(os.path.join(src , file) , os.path.join(os.path.join(des , fname),file))
-						Files_OverWrited += 1
-					except:
-						Files_Skipped += 1
-						pass
-				#Xfname = "Conflicts"
-				#Xdes = "C:\\Users\\Ps_Sarvna\\Desktop\\Des\\Conflicts"
-				#if Xfname not in os.listdir(des):
-				#	os.mkdir(os.path.join(des , Xfname))
-				#if fname not in os.listdir(Xdes):
-				#	os.mkdir(os.path.join(Xdes , fname))
-				#try:	
-				#	shutil.move(os.path.join(src , file) , os.path.join(Xdes , fname))
-				#except:
-
-		else:
-			if Inc_sub:
-				if os.path.isdir(os.path.join(src , file)):
-					Sort_files(os.path.join(src , file),des, Datetime , Inc_sub, Ext_Sort ,Ext_Spec, OverWrite ,Copy )
-					try:
-						os.rmdir(os.path.join(src , file))
-					except:
-						pass
-		
-
 #--------------------------------------------------------------------------------------------------------------------
 
 gui_title = tk.Label(text = "Organize files" , fg = "black", font= ("Fixedsys",24) )
@@ -234,32 +181,22 @@ lineh.place(x =40, y = 295)
 lineh = tk.Canvas(window,width = 500 , height = 1 , bg = "#DCDCDC")
 lineh.place(x =40, y = 395)
 
-Radio_btn1 = tk.Radiobutton(window,text="Sort - Month/Year" , value = "%b_%Y" , variable = daterb,tristatevalue = "x",command = sort_pre)
-Radio_btn1.place(x =100 , y = 130)
-Radio_btn2 = tk.Radiobutton(window,text="Sort - Year      ." , value = "%Y", variable = daterb,tristatevalue = "x",command = sort_pre)
-Radio_btn2.place(x =100 , y = 170)
-Radio_btn3 = tk.Radiobutton(window,text="Sort - Ext       ." , value = "ext", variable = daterb,tristatevalue = "x",command = sort_pre)
-Radio_btn3.place(x =100 , y = 210)
-Radio_btn4 = tk.Radiobutton(window,text="Sort - Ext-Spec  " , value = "extspec", variable = daterb,tristatevalue = "x",command = sort_pre)
-Radio_btn4.place(x =100 , y = 250)
-Radio_btn5 = tk.Radiobutton(window,text="UnSort" , value = "unsort", variable = daterb , command = Pre_unsort,tristatevalue = "x")
-Radio_btn5.place(x =375 , y = 130)
+Combo_sort = ttk.Combobox(window, values = ('Year' , 'Mon_Year' , 'FileType' , 'Extensions'))
+Combo_sort.place(x = 100, y = 150)
 
+Combo_Unsort = ttk.Combobox(window, values = ('Year' , 'Mon_Year' , 'FileType' , 'Extensions'))
+Combo_Unsort.place(x = 100, y = 150)
 
 Move_chec = tk.Checkbutton(text = "Move" , onvalue = False , offvalue  = True , variable = copy)
 Move_chec.place(x =375 , y = 175)
 Copy_chec = tk.Checkbutton(text = "Copy" , onvalue = True , offvalue  = False , variable = copy)
 Copy_chec.place(x =375 , y = 200)
 
-
-
 Inc_chec = tk.Checkbutton(text = "Include Folders" , onvalue = True , offvalue  = False , variable = Inc_check)
 Inc_chec.place(x =375 , y = 225)
 
 Overwrite = tk.Checkbutton(text = "Overwrite Exist files" , onvalue = True , offvalue  = False , variable = Over_check)
 Overwrite.place(x =375 , y = 250)
-
-
 
 Src_entry = tk.Entry(window ,width = 40)
 Src_entry.place(x =50 , y = 320)
